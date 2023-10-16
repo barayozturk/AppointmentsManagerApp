6ad3986413acd46ae76ceda0f6eaaac84e6a5f60
Baray Öztürk <barayozturk58@gmail.com>
2023-10-16 16:08:32 +0300

    first commit

---
+++ AppointmentsManager/Data/Models/Apointment.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppointmentsManager.Data.Models
+{
+    public class Apointment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(150), Column(TypeName ="nvarchar(150)")]
+        public string Title { get; set; }
+        
+        [MaxLength(300), Column(TypeName = "nvarchar(300)")]
+        public string Description { get; set; }
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;  
+        public DateTime Date { get; set; } = DateTime.Now;
+        
+        [MaxLength(100), Column(TypeName = "nvarchar(100)")]
+        public string Address { get; set; } = "Address";
+
+        [MaxLength(10), Column(TypeName = "nvarchar(10)")]
+        public string Time { get; set; } = "12:30";
+        public bool Done { get; set; } = false;
+        public bool Deleted { get; set; } = false;
+        public byte LevelOfImportance { get; set; } = 2;
+    }
+}
